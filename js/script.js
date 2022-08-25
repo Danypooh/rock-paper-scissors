6302337fd5b60d051419f7e91455fd58e37d2b46
Daniel <delatorrechapell@gmail.com>
2022-08-24 23:33:30 -0500

    Rock paper scissors with js done
    forgot to commit early and often...again

--- js/script.js
+++ js/script.js
@@ -2,66 +2,96 @@
 let match;
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
+
+let container = document.querySelector('div');
 
 function getComputerChoice() {
     let adversary = Math.floor(Math.random() * 3);
     switch (adversary) {
         case 0:
-            return 'Rock';
+            return 'rock';
         case 1:
-            return 'Paper';
+            return 'paper';
         case 2:
-            return 'Scissors';
+            return 'scissors';
         default:
             console.log('Sorry something went wrong');
     }
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
     if (playerSelection=='rock' && computerSelection=='paper') {
         computerScore ++;
-        return match = "You Lose! Paper beats Rock";
+        match = "You Lose! Paper beats Rock";
     }
     else if (playerSelection=='rock' && computerSelection=='scissors') {
         playerScore ++;
-        return match = "You Win! Rock beats Scissors";
+        match = "You Win! Rock beats Scissors";
     }
     else if (playerSelection=='paper' && computerSelection=='rock') {
         playerScore ++;
-        return match = "You Win! Paper beats Rock";
+        match = "You Win! Paper beats Rock";
     }
     else if (playerSelection=='paper' && computerSelection=='scissors') {
         computerScore ++;
-        return match = "You Lose! Scissors beats Paper";
+        match = "You Lose! Scissors beats Paper";
     }
     else if (playerSelection=='scissors' && computerSelection=='rock') {
         computerScore ++;
-        return match = "You Lose! Rock beats Scissors";
+        match = "You Lose! Rock beats Scissors";
     }
     else if (playerSelection=='scissors' && computerSelection=='paper') {
         playerScore ++;
-        return match = "You Win! Scissors beats Paper";
+        match = "You Win! Scissors beats Paper";
     }
     else {
-        return match = "It's a tie!";
+        match = "It's a tie!";
     }
+    printCurrentResults();
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = window.prompt("Enter Rock, Paper or Scissors: ");
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection.toLowerCase(), computerSelection.toLowerCase());
-        console.log(match);
-        console.log("your score: " + playerScore);
-        console.log("computer score: " + computerScore);
-    }
+function printCurrentResults() {
+  let roundResult = document.createElement('div');
+  roundResult.textContent = match;
+  container.appendChild(roundResult);
+  let playerCurrentScore = document.createElement('div');
+  playerCurrentScore.textContent = "Player Score: " + playerScore + '\n';
+  container.appendChild(playerCurrentScore);
+  let computerCurrentScore = document.createElement('div');
+  computerCurrentScore.textContent = "Computer Score: " + computerScore + '\n';
+  container.appendChild(computerCurrentScore);
+  round ++;
+  if (round == 5) {
+      printFinalResult();
+  }
 }
 
-game();
-if (playerScore > computerScore) {
-    console.log("Congratulations! You won!");
+function printFinalResult() {
+  let finalScore = document.createElement('div');
+  if (playerScore > computerScore) {
+    finalScore.textContent = "\n ***Congratulations! You won!***"
+  }
+  else {
+    finalScore.textContent = "\n ***Sorry! You lose!***";
+  }
+  container.appendChild(finalScore);
+  restart();
 }
-else {
-    console.log("Sorry! You lose!");
+
+function restart() {
+  playerScore = 0;
+  computerScore = 0;
+  round = 0;
+  //document.getElementsByTagName('div').value = ""; 
 }
+
+function bootGame() {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach(button => button.addEventListener('click', function () { 
+    playRound(button.textContent);
+  }));
+}
+
+bootGame();
